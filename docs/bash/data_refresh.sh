8940b057a74465405bddf9442962401aba07d08b
benthecoder <benthecoder07@gmail.com>
2021-02-21 14:46:48 +0800

    updated dashboard

---
+++ docs/bash/data_refresh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Rendering the dashboard..."
+
+Rscript -e "rmarkdown::render_site()"
+
+if [[ "$(git status --porcelain)" != "" ]]; then
+    git config --global user.name 'benthecoder'
+    git config --global user.email 'benthecoder07@gmail.com'
+    git add *
+    git commit -m "Auto update dashboard"
+    git push
+fi
